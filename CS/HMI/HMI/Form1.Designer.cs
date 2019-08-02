d2bbef3dbba6672b095842e54dd046c887684f85
Jack <hqc821@163.com>
2019-08-02 21:54:39 +0800

    add stop button to CS,still need more coding

--- CS/HMI/HMI/Form1.Designer.cs
+++ CS/HMI/HMI/Form1.Designer.cs
@@ -29,21 +29,35 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btnPowerOn = new System.Windows.Forms.Button();
             this.btnPowerOff = new System.Windows.Forms.Button();
+            this.btnPowerOn = new System.Windows.Forms.Button();
+            this.BtnPowerStatus = new System.Windows.Forms.Button();
+            this.BtnStop = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.BtnStop);
+            this.groupBox1.Controls.Add(this.BtnPowerStatus);
             this.groupBox1.Controls.Add(this.btnPowerOff);
             this.groupBox1.Controls.Add(this.btnPowerOn);
             this.groupBox1.Location = new System.Drawing.Point(14, 9);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(305, 334);
+            this.groupBox1.Size = new System.Drawing.Size(548, 187);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
+            // 
+            // btnPowerOff
+            // 
+            this.btnPowerOff.Location = new System.Drawing.Point(138, 21);
+            this.btnPowerOff.Name = "btnPowerOff";
+            this.btnPowerOff.Size = new System.Drawing.Size(126, 35);
+            this.btnPowerOff.TabIndex = 1;
+            this.btnPowerOff.Text = "AxisPowerOff";
+            this.btnPowerOff.UseVisualStyleBackColor = true;
+            this.btnPowerOff.Click += new System.EventHandler(this.btnPowerOff_Click);
             // 
             // btnPowerOn
             // 
@@ -55,15 +69,25 @@
             this.btnPowerOn.UseVisualStyleBackColor = true;
             this.btnPowerOn.Click += new System.EventHandler(this.btnPowerOn_Click);
             // 
-            // btnPowerOff
+            // BtnPowerStatus
             // 
-            this.btnPowerOff.Location = new System.Drawing.Point(138, 21);
-            this.btnPowerOff.Name = "btnPowerOff";
-            this.btnPowerOff.Size = new System.Drawing.Size(126, 35);
-            this.btnPowerOff.TabIndex = 1;
-            this.btnPowerOff.Text = "AxisPowerOff";
-            this.btnPowerOff.UseVisualStyleBackColor = true;
-            this.btnPowerOff.Click += new System.EventHandler(this.btnPowerOff_Click);
+            this.BtnPowerStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPowerStatus.Location = new System.Drawing.Point(282, 17);
+            this.BtnPowerStatus.Name = "BtnPowerStatus";
+            this.BtnPowerStatus.Size = new System.Drawing.Size(165, 43);
+            this.BtnPowerStatus.TabIndex = 3;
+            this.BtnPowerStatus.Text = "PowerStatus";
+            this.BtnPowerStatus.UseVisualStyleBackColor = true;
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Location = new System.Drawing.Point(6, 74);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(126, 35);
+            this.BtnStop.TabIndex = 4;
+            this.BtnStop.Text = "AxisStop";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
             // 
             // Form1
             // 
@@ -85,6 +109,8 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnPowerOn;
         private System.Windows.Forms.Button btnPowerOff;
+        private System.Windows.Forms.Button BtnPowerStatus;
+        private System.Windows.Forms.Button BtnStop;
     }
 }
 
--- CS/HMI/HMI/Form1.cs
+++ CS/HMI/HMI/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TwinCAT.Ads;
+using System.Threading;
 
 namespace HMI
 {
@@ -15,6 +16,9 @@ namespace HMI
     {
         TcAdsClient client;
         int hPowerOn;
+        int hPowerMonitor;
+        int hStop;
+        bool bPowerOn;
 
         public Form1()
         {
@@ -29,29 +33,58 @@ namespace HMI
             client.Connect(851);
             try
             {
-              hPowerOn =  client.CreateVariableHandle("GVL_General.bPowerOn");
+                hPowerOn =  client.CreateVariableHandle("GVL_General.hmiPowerOn");
+                hPowerMonitor = client.CreateVariableHandle("GVL_General.bPowrOn");
+                hStop = client.CreateVariableHandle("GVL_General.hmiStop");
+
+                client.AddDeviceNotificationEx("GVL_General.bPowrOn", AdsTransMode.OnChange, 100, 0, BtnPowerStatus, typeof(Boolean));
+
+                client.AdsNotificationEx += Client_AdsNotificationEx;
+                
             }
             catch (Exception err)
             {
                 MessageBox.Show("Form1_Load " + err.Message);                
             }
-
         }
 
+        private void Client_AdsNotificationEx(object sender, AdsNotificationExEventArgs e)
+        {
+            if (e.Value.GetType() == typeof(Boolean))
+            {
+                bPowerOn = (Boolean)e.Value;
+            }
+            AxisPowerIndicatior();
+        } 
+        private void AxisPowerIndicatior()
+        {
+            if (bPowerOn)
+            {
+                BtnPowerStatus.BackColor = Color.Green;
+                BtnPowerStatus.Text = "PowerOn";
+            }
+            else
+            {
+                BtnPowerStatus.BackColor = Color.Gray;
+                BtnPowerStatus.Text = "PowerOff";
+
+            }
+        }
         private void btnPowerOn_Click(object sender, EventArgs e)
         {
             try
             {
+                client.WriteAny(hStop, false);
+
                 client.WriteAny(hPowerOn, true);
                 btnPowerOn.Enabled = false;
-                btnPowerOff.Enabled = true;
+                btnPowerOff.Enabled = true;                
             }
             catch (Exception err)
             {
                 MessageBox.Show("btnPowerOn_Click " + err.Message);
                 //throw;
-            }           
-
+            } 
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -65,13 +98,29 @@ namespace HMI
             {
                 client.WriteAny(hPowerOn, false);
                 btnPowerOn.Enabled = true;
-                btnPowerOff.Enabled = false;
+                btnPowerOff.Enabled = false;                
+
             }
             catch (Exception err)
             {
                 MessageBox.Show("btnPowerOn_Click " + err.Message);
                 //throw;
             }
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client.WriteAny(hStop, true);
+                //Thread.Sleep(1000);
+                //client.WriteAny(hStop, false);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("BtnStop_Click " + err.Message);
+            }
+
         }
     }
 }
