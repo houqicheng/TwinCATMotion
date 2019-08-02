42142c7f4dedda2c4b488625c0bafcb28c1f98bc
Jack <hqc821@163.com>
2019-08-02 08:09:13 +0800

    add PowerON/Off button to HMI

--- CS/HMI/HMI/Form1.Designer.cs
+++ CS/HMI/HMI/Form1.Designer.cs
@@ -29,28 +29,41 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnPowerOn = new System.Windows.Forms.Button();
+            this.btnPowerOff = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnPowerOff);
+            this.groupBox1.Controls.Add(this.btnPowerOn);
             this.groupBox1.Location = new System.Drawing.Point(14, 9);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(240, 334);
+            this.groupBox1.Size = new System.Drawing.Size(305, 334);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
-            // button1
+            // btnPowerOn
             // 
-            this.button1.Location = new System.Drawing.Point(6, 21);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 35);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "AxisPowerOn";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnPowerOn.Location = new System.Drawing.Point(6, 21);
+            this.btnPowerOn.Name = "btnPowerOn";
+            this.btnPowerOn.Size = new System.Drawing.Size(126, 35);
+            this.btnPowerOn.TabIndex = 0;
+            this.btnPowerOn.Text = "AxisPowerOn";
+            this.btnPowerOn.UseVisualStyleBackColor = true;
+            this.btnPowerOn.Click += new System.EventHandler(this.btnPowerOn_Click);
+            // 
+            // btnPowerOff
+            // 
+            this.btnPowerOff.Location = new System.Drawing.Point(138, 21);
+            this.btnPowerOff.Name = "btnPowerOff";
+            this.btnPowerOff.Size = new System.Drawing.Size(126, 35);
+            this.btnPowerOff.TabIndex = 1;
+            this.btnPowerOff.Text = "AxisPowerOff";
+            this.btnPowerOff.UseVisualStyleBackColor = true;
+            this.btnPowerOff.Click += new System.EventHandler(this.btnPowerOff_Click);
             // 
             // Form1
             // 
@@ -60,6 +73,8 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "TwinCATMotion";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -68,7 +83,8 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPowerOn;
+        private System.Windows.Forms.Button btnPowerOff;
     }
 }
 
--- CS/HMI/HMI/Form1.cs
+++ CS/HMI/HMI/Form1.cs
@@ -7,14 +7,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TwinCAT.Ads;
 
 namespace HMI
 {
     public partial class Form1 : Form
     {
+        TcAdsClient client;
+        int hPowerOn;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnPowerOn.Enabled = true;
+            btnPowerOff.Enabled = false;
+            client = new TcAdsClient();
+            client.Connect(851);
+            try
+            {
+              hPowerOn =  client.CreateVariableHandle("GVL_General.bPowerOn");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Form1_Load " + err.Message);                
+            }
+
+        }
+
+        private void btnPowerOn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client.WriteAny(hPowerOn, true);
+                btnPowerOn.Enabled = false;
+                btnPowerOff.Enabled = true;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("btnPowerOn_Click " + err.Message);
+                //throw;
+            }           
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            client.Dispose();
+        }
+
+        private void btnPowerOff_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client.WriteAny(hPowerOn, false);
+                btnPowerOn.Enabled = true;
+                btnPowerOff.Enabled = false;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("btnPowerOn_Click " + err.Message);
+                //throw;
+            }
         }
     }
 }
